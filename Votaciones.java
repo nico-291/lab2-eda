74ec7247406264437afa2cce0f8ed630bf90e6d5
Nicolas Hermosilla <nicolas.hermosilla1@mail.udp.cl>
2025-04-26 23:34:51 -0400

    Update Votaciones.java

--- Votaciones.java
+++ Votaciones.java
@@ -1,28 +1,36 @@
-import java.util.LinkedList;
-import java.util.Map;
-import java.util.Queue;
-import java.util.Stack;
-
+import java.util.*;
+import java.util.Scanner;
 public class Votaciones{
     public class Voto{
-    private int id;
-    private int votanteId;
-    private int candidatoId;
-    private String timeStamp;
-    public Voto(int id, int votanteId, int candidatoId){
-        this.id = id;
-        this.votanteId = votanteId;
-        this.candidatoId = candidatoId;
-        this.timeStamp = timeStamp;
-    }
-    public int getId(){return id;}
-    public int getVotanteId(){return votanteId;}
-    public int getCandidatoId(){return candidatoId;}
-    public String getTimeStamp(){return timeStamp;}
-    public void setId(int id){this.id = id;}
-    public void setVotanteId(int votanteId){this.votanteId = votanteId;}
-    public void (int candidatoId){this.candidatoId = candidatoId;}
-    public void setTimeStamp(String timeStamp){this.timeStamp = timeStamp;}
+        private int id;
+        private int votanteId;
+        private int candidatoId;
+        private String timeStamp;
+        public Voto(int id, int votanteId, int candidatoId){
+            this.id = id;
+            this.votanteId = votanteId;
+            this.candidatoId = candidatoId;
+            this.timeStamp = new java.util.Date().toString();
+        }
+
+        //get set
+        public int getId(){
+            return id;}
+        public int getVotanteId(){
+            return votanteId;}
+        public int getCandidatoId(){
+            return candidatoId;}
+        public String getTimeStamp(){
+            return timeStamp;}
+
+        public void setId (int id){
+            this.id = id;}
+        public void setVotanteId (int votanteId){
+            this.votanteId = votanteId;}
+        public void setCandidatoId (int candidatoId){
+            this.candidatoId = candidatoId;}
+        public void setTimeStamp(String timeStamp){
+            this.timeStamp = timeStamp;}
     }
 
 
@@ -31,20 +39,31 @@ public class Votaciones{
         private String nombre;
         private String partido;
         private Queue<Voto> votoRecibidos;
+
         public Candidato(int id, String nombre, String partido){
             this.id = id;
             this.nombre = nombre;
             this.partido = partido;
             votoRecibidos = new LinkedList<>();
-    }
-        public int getId(){return id;}
-        public String getNombre(){return nombre;}
-        public String getPartido(){return partido;}
-        public Queue<Voto> getVotosRecibidos(){return votoRecibidos;}
-        public void setId(int id){this.id = id;}
-        public void setNombre(String nombre){this.nombre = nombre;}
-        public void setPartido(String partido){this.partido = partido;}
-        public void agregarVoto(Voto v){votoRecibidos.add(v);}
+        }
+        //get set
+        public int getId(){
+            return id;}
+        public String getNombre(){
+            return nombre;}
+        public String getPartido(){
+            return partido;}
+        public Queue<Voto> getVotosRecibidos(){
+            return votoRecibidos;}
+
+        public void setId(int id){
+            this.id = id;}
+        public void setNombre(String nombre){
+            this.nombre = nombre;}
+        public void setPartido(String partido){
+            this.partido = partido;}
+        public void agregarVoto(Voto v){
+            votoRecibidos.add(v); }
     }
 
 
@@ -53,20 +72,26 @@ public class Votaciones{
         private String nombre;
         private boolean yaVoto;
         public Votante(int id, String nombre, boolean yaVoto){
-         this.id = id;
-         this.nombre = nombre;
-         this.yaVoto = yaVoto;
-    }
-        public int getId(){return id;}
-        public String getNombre(){return nombre;}
-        public boolean getYaVoto(){return yaVoto;}
-        public void setId(int id){this.id = id;}
-        public void setNombre(String nombre){this.nombre = nombre;}
-        public void setYaVoto(boolean yaVoto){this.yaVoto = yaVoto;}
-        public void marcarVotado(){
-            this.yaVoto = true;
+            this.id = id;
+            this.nombre = nombre;
+            this.yaVoto = yaVoto;
         }
+        //get set
+        public int getId(){
+            return id;}
+        public String getNombre(){
+            return nombre;}
+        public boolean getYaVoto(){
+            return yaVoto;}
 
+        public void setId(int id){
+            this.id = id;}
+        public void setNombre(String nombre){
+            this.nombre = nombre;}
+        public void setYaVoto(boolean yaVoto){
+            this.yaVoto = yaVoto;}
+        public void marcarVotado(){
+            this.yaVoto = true;}
     }
 
 
@@ -75,6 +100,7 @@ public class Votaciones{
         private Stack <Voto> historialVotos;
         private Queue<Voto> votosReportados;
         private int idCounter;
+
         public UrnaElectoral(){
             this.Listacandidatos = new LinkedList<>();
             this.historialVotos = new Stack<>();
@@ -82,44 +108,74 @@ public class Votaciones{
             this.idCounter = 1;
         }
 
-        public void agregarCandidato(Candidato candidato){Listacandidatos.add(candidato)}
-        public boolean verificarVotante(Votante votante){return votante.getYaVoto();}
+        public void agregarCandidato(Candidato candidato){
+            Listacandidatos.add(candidato);
+        }
 
-        public boolean registrtarVoto(Votante votante, int candidatoId){
+        public boolean verificarVotante(Votante elvotante){
+            return elvotante.getYaVoto();}
+
+        public boolean registrarVoto(Votante votante, int candidatoId){
+
             if(verificarVotante(votante)){
-                System.out.println("El votante"+ votante.getId() + " ya ah votado");
+                System.out.println("El votante"+ votante.getId() + " ya voto");
+                return false;
             }
-            for (Candidato candidato : Listacandidatos) {
+            for (int i=0; i<Listacandidatos.size(); i++) {
+                Candidato candidato = Listacandidatos.get(i);
+
                 if (candidato.getId() == candidatoId) {
                     Voto nuevoVoto=new Voto(idCounter++,votante.getId(),candidatoId);
                     candidato.agregarVoto(nuevoVoto);
                     historialVotos.add(nuevoVoto);
                     votante.marcarVotado();
-                    System.out.println("Voto registrado para el votante"+ candidato.getNombre());
+                    System.out.println("EL voto a sido registrado (voto para)"+ candidato.getNombre());
+                    return true;
                 }
-                System.out.println("candidato no encontrado");
-                return false;
+
             }
+            System.out.println("candidato no encontrado");
+            return false;
         }
+
         public boolean reportarVoto(Candidato candidato,int idVoto){
-            Queue<Voto> votosCandidato= candidato.getVotosRecibidos();
-            Voto votoReportado=null;
-            for(Voto voto : votosCandidato){
+            Queue<Voto> votosCandidato = candidato.getVotosRecibidos();
+            List<Voto> listaVotos = new ArrayList<>(votosCandidato);
+            Voto votoReportado = null;
+
+            for(int i=0; i<votosCandidato.size(); i++){
+                Voto voto = listaVotos.get(i);
                 if(voto.getId() == idVoto){
-                    votoReportado=voto;
+
+                    votoReportado = voto;
                     break;
                 }
             }
-            if (votoReportado!=null){
+
+            if (votoReportado != null){
                 votosCandidato.remove(votoReportado);
                 votosReportados.add(votoReportado);
                 System.out.println("Voto reportado registrado");
+                return true;
             }else {
-                System.out.println("Voto no encontrado en el registro");
+                System.out.println("Voto no encontrado");
+                return false;
             }
         }
-        public Map<String,Integer> obtenerResultado(){
 
+        public Map<String, Integer> obtenerResultados() {
+            Map<String, Integer> resultados = new HashMap<>();
+
+            for (int i = 0; i < Listacandidatos.size(); i++) {
+                Candidato candidato = Listacandidatos.get(i);
+
+                int totalVotos = candidato.getVotosRecibidos().size();
+
+                resultados.put(candidato.getNombre(), totalVotos);
+            }
+
+            resultados.put("Votos Reportados", votosReportados.size());
+            return resultados;
         }
     }
 }
